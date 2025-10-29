eb77060447848d6e9f970dcb690ffc3456e6cda8
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-10-29 11:33:26 +0600

    handle delete operation and remove user from the ui without page reload.

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FaPen } from "react-icons/fa";
 import { ImBin2 } from "react-icons/im";
 import { IoMdEye } from "react-icons/io";
@@ -5,12 +6,27 @@ import { useLoaderData } from "react-router";
 
 const Users = () => {
 	const initialUsers = useLoaderData();
+	const [users, setUsers] = useState(initialUsers);
+
+	// Delete User
+	const handleDeleteUser = (id) => {
+		fetch(`http://localhost:3000/users/${id}`, {
+			method: "delete",
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.deletedCount) {
+					const remainingUsers = users.filter(
+						(user) => user._id !== id
+					);
+					setUsers(remainingUsers);
+				}
+			});
+	};
 
 	return (
 		<div className="container py-10!">
-			<h2 className="mb-4 text-center">
-				Total Users : {initialUsers.length}
-			</h2>
+			<h2 className="mb-4 text-center">Total Users : {users.length}</h2>
 			<div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
 				<table className="table">
 					{/* head */}
@@ -24,7 +40,7 @@ const Users = () => {
 					</thead>
 					<tbody>
 						{/* row 1 */}
-						{initialUsers.map((user, idx) => (
+						{users.map((user, idx) => (
 							<tr key={user._id}>
 								<th>
 									{idx + 1 < 10 ? `0${idx + 1}` : idx + 1}
@@ -38,7 +54,12 @@ const Users = () => {
 									<button className="bg-[#DDA000] h-9 px-2 rounded cursor-pointer">
 										<FaPen className="text-[20px] text-white" />
 									</button>
-									<button className="bg-red-500 h-9 px-2 rounded cursor-pointer">
+									<button
+										onClick={() =>
+											handleDeleteUser(user._id)
+										}
+										className="bg-red-500 h-9 px-2 rounded cursor-pointer"
+									>
 										<ImBin2 className="text-[20px] text-white" />
 									</button>
 								</td>
